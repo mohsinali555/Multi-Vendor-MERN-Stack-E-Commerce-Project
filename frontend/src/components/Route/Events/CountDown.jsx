6a7e93e26d6728bc0fc7447097b70b5e5e906aad
Subhani55 <m.mohsinalisubhani5@gmail.com>
2025-06-29 23:36:51 +0500

    add events section in homepage

---
+++ frontend/src/components/Route/Events/CountDown.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const CountDown = () => {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  function calculateTimeLeft() {
+    const difference = +new Date("13-0-2025") - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  }
+
+  return <div>CountDown</div>;
+};
+
+export default CountDown;
