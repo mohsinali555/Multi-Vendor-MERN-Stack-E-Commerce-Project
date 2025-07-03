25ce76e2fd32ef1dfe9f4b2d184d7745935af225
Subhani55 <m.mohsinalisubhani5@gmail.com>
2025-07-03 20:44:10 +0500

    add cart component in the header

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const Cart = () => {
-  return <div>Cart</div>;
+const Cart = ({ setOpenCart }) => {
+  const cartData = [
+    {
+      name: "Iphone 14 pro max 256 gb ssd and 8 gb ram silver colour",
+      description: "test",
+      price: "999",
+    },
+    {
+      name: "Iphone 14 pro max 256 gb ssd and 8 gb ram silver colour",
+      description: "test",
+      price: "245",
+    },
+    {
+      name: "Iphone 14 pro max 256 gb ssd and 8 gb ram silver colour",
+      description: "test",
+      price: "645",
+    },
+  ];
+
+  return (
+    <div classname="fixed top-0 left-0 w-full bg-[#0000004b] h-screen z-10"></div>
+  );
 };
 
 export default Cart;
