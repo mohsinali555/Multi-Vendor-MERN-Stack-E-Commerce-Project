35f5130ae286d62bef697311f5d314efe8c20df1
Subhani55 <m.mohsinalisubhani5@gmail.com>
2025-07-06 21:08:51 +0500

    add productDtails component in productDetails Page

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import styles from "../../styles/styles";
 
-const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+const ProductDetails = ({ data }) => {
+  const [count, setCount] = useState(1);
+  const [click, setClick] = useState(false);
+  const navigate = useNavigate();
+  return (
+    <div className="bg-white">
+      {data ? (
+        <div
+          className={`${styles.section} w-[90%] min-[800px]:w-[80%] h-screen`}
+        >
+          <div className="w-full py-5">
+            <div className="block w-full min-[800px]:flex">
+              <div className="w-full min-[800px]:w-[50%]">
+                <div className="w-full flex">
+                  <div
+                    className={`${
+                      select === 0 ? "border" : "null"
+                    } cursor-pointer`}
+                  >
+                    <img src="" alt="" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default ProductDetails;
