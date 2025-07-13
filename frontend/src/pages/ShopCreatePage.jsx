c7b61a91788e961bbecb0c2da489786b70217f6c
Subhani55 <m.mohsinalisubhani5@gmail.com>
2025-07-13 22:21:35 +0500

    seller auth,login, load seller completed

--- frontend/src/pages/ShopCreatePage.jsx
+++ frontend/src/pages/ShopCreatePage.jsx
@@ -1,5 +1,17 @@
 import ShopCreate from "../components/Shop/ShopCreate";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 const ShopCreatePage = () => {
+  const navigate = useNavigate();
+  const { isSeller, seller } = useSelector((state) => state.seller);
+
+  useEffect(() => {
+    if (isSeller === true) {
+      navigate(`/shop/${seller._id}`);
+    }
+  }, []);
   return (
     <div>
       <ShopCreate />
